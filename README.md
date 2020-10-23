09f956f8ddb281bb4d179408d92143ab35790606
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-10-23 23:20:29 +0100

    Adapt publico spider to handle more news sites

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # NewsAgreggator
 
-Generic crawler
+`$ cd spiders`
+
+<br/>
+
+`Generic crawler`
+
 Usage: scrapy crawl Generic -o generic.json
+
+The result will be output to `generic.json`
+
+<br/>
+
+`Ctw and Bmw crawler`
+
+`$ scrapy crawl ctwAndBmw`
+
+The result will be output to `NewsResult.txt`
 
---
+++ ctwCrawler/spiders/ctwAndBmw.py
@@ -0,0 +1,55 @@
+import scrapy
+
+class PublicoSpider(scrapy.Spider):
+    name = 'ctwAndBmw'
+
+    urls = [
+        'https://www.jn.pt',
+        'https://www.dn.pt',
+        'https://www.observador.pt',
+        'https://www.rtp.pt/noticias',
+        'https://www.noticiasaominuto.com',
+        'https://www.publico.pt',
+        'https://www.expresso.pt',
+        'https://www.jornaldenegocios.pt',
+    ]
+
+    patterns_url = [
+        '//a[contains(@href, "bmw")]/@href',
+        '//a[contains(@href, "BMW")]/@href',
+        '//a[contains(@href, "Ctw")]/@href'
+        '//a[contains(@href, "ctw")]/@href'
+        '//a[contains(@href, "critical-techworks")]/@href'
+        '//a[contains(@href, "criticaltechworks")]/@href'
+    ]
+
+    def prepend(self, title):
+        switcher={
+            'Jornal de Notícias':"https://www.jn.pt",
+            'Diário de Notícias':"https://www.dn.pt",
+            'PÚBLICO — Pense bem, pense Público':"https://www.publico.pt",
+            'Expresso | Liberdade para pensar':"https://www.expresso.pt",
+            'Negócios: Cotações, Mercados, Economia, Empresas':"https://www.jornaldenegocios.pt",
+         }
+        return switcher.get(title,"")
+
+    def start_requests(self):
+        for url in self.urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        siteTitle = response.css('title::text').extract_first()
+        prefix = self.prepend(title=siteTitle)
+
+        with open('NewsResult.txt', 'a') as f:
+            inserted = []
+            for pattern in self.patterns_url:
+                links = response.xpath(pattern).getall()
+
+                for link in links:
+                    if link not in inserted and '#comments' not in link:
+                        inserted.append(link)
+                        f.write(prefix + link + '\n')
+                        yield {
+                            "link": prefix + link,
+                        }
